2f210953b6c41b4c75e942bc2e8f685ec8bfb906
AryanNair-24 <aryan.nair@mail.utoronto.ca>
2024-11-23 14:42:36 -0500

    Implemented MoonPhasePresenter.java and created and implemented Controller, Presenter and View for DisplayMoonPhase

--- src/main/java/interface_adapter/moon_phase/MoonPhasePresenter.java
+++ src/main/java/interface_adapter/moon_phase/MoonPhasePresenter.java
@@ -1,13 +1,31 @@
 package interface_adapter.moon_phase;
 
 import entity.MoonPhase;
+import view.DisplayMoonPhaseView;
+import view.ViewManager;
+
+import java.io.IOException;
 
 public class MoonPhasePresenter {
-    public static void back() {
+    private ViewManager viewManager;
+    private DisplayMoonPhaseView displayMoonPhaseView;
 
+    public MoonPhasePresenter(ViewManager viewManager){
+        this.viewManager = viewManager;
     }
 
-    public void displayMoonPhase(MoonPhase moonPhase) {
+    public void setDisplayMoonPhaseView(DisplayMoonPhaseView displayMoonPhaseView){
+        this.displayMoonPhaseView = displayMoonPhaseView;
+    }
 
+    public void displayMoonPhase(MoonPhase moonPhase) throws IOException {
+        displayMoonPhaseView.setMoonPhase(moonPhase);
+        displayMoonPhaseView.refresh();
+        viewManager.getViews().add(displayMoonPhaseView, "display Moon Phase");
+        viewManager.show("display Moon Phase");
+    }
+
+    public void back() {
+        viewManager.show("home");
     }
 }
