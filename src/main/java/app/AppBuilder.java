60cf58a65d49c1524d16ba806642fc705c3d7da8
alexxxtl <alextl.young@mail.utoronto.ca>
2024-11-25 23:32:36 -0500

    Added use case in appbuilder

--- src/main/java/app/AppBuilder.java
+++ src/main/java/app/AppBuilder.java
@@ -1,24 +1,16 @@
 package app;
 
-import data_access.EventsDataAccessObject;
-import data_access.MoonPhaseDataAccessObject;
+import data_access.GeocoderDataAccessObject;
 import data_access.StarChartDataAccessObject;
-import interface_adapter.display_events.DisplayEventsController;
-import interface_adapter.display_events.DisplayEventsPresenter;
-import interface_adapter.display_moon_phase.DisplayMoonPhaseController;
-import interface_adapter.display_moon_phase.DisplayMoonPhasePresenter;
 import interface_adapter.display_star_chart.DisplayStarChartController;
 import interface_adapter.display_star_chart.DisplayStarChartPresenter;
-import interface_adapter.events.EventsController;
-import interface_adapter.events.EventsPresenter;
+import interface_adapter.geocoding.GeocodingController;
+import interface_adapter.geocoding.GeocodingPresenter;
 import interface_adapter.home.HomeController;
 import interface_adapter.home.HomePresenter;
-import interface_adapter.moon_phase.MoonPhaseController;
-import interface_adapter.moon_phase.MoonPhasePresenter;
 import interface_adapter.star_chart.StarChartController;
 import interface_adapter.star_chart.StarChartPresenter;
-import use_case.events.EventsInteractor;
-import use_case.moon_phase.MoonPhaseInteractor;
+import use_case.geocoding.GeocodingInteractor;
 import use_case.star_chart.StarChartInteractor;
 import view.*;
 
@@ -30,15 +22,13 @@ public class AppBuilder {
     private final CardLayout cardLayout = new CardLayout();
     private final ViewManager viewManager = new ViewManager(cardLayout, cardPanel);
     private final StarChartDataAccessObject starChartDAO = new StarChartDataAccessObject();
-    private final EventsDataAccessObject eventsDAO = new EventsDataAccessObject();
-    private final MoonPhaseDataAccessObject moonPhaseDAO = new MoonPhaseDataAccessObject();
+    private final GeocoderDataAccessObject geocoderDAO = new GeocoderDataAccessObject();
     private HomeView homeView;
     private StarChartView starChartView;
-    private MoonPhaseView moonPhaseView;
     private DisplayStarChartView displayStarChartView;
-    private EventsView eventsView;
-    private DisplayEventsView displayEventsView;
-    private DisplayMoonPhaseView displayMoonPhaseView;
+    private GeocoderView geocoderView;
+    private DisplayGeocoderView displayGeocoderView;
+    private NoAddressFoundView noAddressFoundView;
 
     public AppBuilder() {
         cardPanel.setLayout(cardLayout);
@@ -56,33 +46,9 @@ public class AppBuilder {
         return this;
     }
 
-    public AppBuilder addMoonPhaseView() {
-        moonPhaseView = new MoonPhaseView();
-        cardPanel.add(moonPhaseView, moonPhaseView.getViewName());
-        return this;
-    }
-
     public AppBuilder addDisplayStarChartView() {
         displayStarChartView = new DisplayStarChartView();
         cardPanel.add(displayStarChartView, displayStarChartView.getViewName());
-        return this;
-    }
-
-    public AppBuilder addEventsView() {
-        eventsView = new EventsView();
-        cardPanel.add(eventsView, eventsView.getViewName());
-        return this;
-    }
-
-    public AppBuilder addDisplayEventsView() {
-        displayEventsView = new DisplayEventsView();
-        cardPanel.add(displayEventsView, displayEventsView.getViewName());
-        return this;
-    }
-
-    public AppBuilder addDisplayMoonPhaseView() {
-        displayMoonPhaseView = new DisplayMoonPhaseView();
-        cardPanel.add(displayMoonPhaseView, displayMoonPhaseView.getViewName());
         return this;
     }
 
@@ -103,16 +69,6 @@ public class AppBuilder {
         return this;
     }
 
-    public AppBuilder addMoonPhaseUseCase() {
-        MoonPhasePresenter moonPhasePresenter = new MoonPhasePresenter(viewManager);
-        moonPhasePresenter.setDisplayMoonPhaseView(displayMoonPhaseView);
-        MoonPhaseInteractor moonPhaseInteractor = new MoonPhaseInteractor(moonPhaseDAO, moonPhasePresenter);
-        MoonPhaseController moonPhaseController = new MoonPhaseController(moonPhaseInteractor);
-        moonPhaseView.setMoonPhaseController(moonPhaseController);
-
-        return this;
-    }
-
     public AppBuilder addDisplayStarChartInterface() {
         DisplayStarChartPresenter displayStarChartPresenter = new DisplayStarChartPresenter(viewManager);
         DisplayStarChartController displayStarChartController = new DisplayStarChartController(displayStarChartPresenter);
@@ -120,26 +76,35 @@ public class AppBuilder {
         return this;
     }
 
-    public AppBuilder addEventsUseCase() {
-        EventsPresenter eventsPresenter = new EventsPresenter(viewManager);
-        eventsPresenter.setDisplayEventsView(displayEventsView);
-        EventsInteractor eventsInteractor = new EventsInteractor(eventsDAO, eventsPresenter);
-        EventsController eventsController = new EventsController(eventsInteractor);
-        eventsView.setEventsController(eventsController);
+    public AppBuilder addGeocodingView() {
+        geocoderView = new GeocoderView();
+        cardPanel.add(geocoderView, geocoderView.getViewName());
         return this;
     }
 
-    public AppBuilder addDisplayEventsInterface() {
-        DisplayEventsPresenter displayEventsPresenter = new DisplayEventsPresenter(viewManager);
-        DisplayEventsController displayEventsController = new DisplayEventsController(displayEventsPresenter);
-        displayEventsView.setDisplayEventsController(displayEventsController);
+    public AppBuilder addDisplayGeocoderView() {
+        displayGeocoderView = new DisplayGeocoderView();
+        cardPanel.add(displayGeocoderView, displayGeocoderView.getViewName());
         return this;
     }
 
-    public AppBuilder addDisplayMoonPhaseInterface() {
-        DisplayMoonPhasePresenter displayMoonPhasePresenter = new DisplayMoonPhasePresenter(viewManager);
-        DisplayMoonPhaseController displayMoonPhaseController = new DisplayMoonPhaseController(displayMoonPhasePresenter);
-        displayMoonPhaseView.setDisplayMoonPhaseController(displayMoonPhaseController);
+    public AppBuilder addNoAddressFoundView() {
+        noAddressFoundView = new NoAddressFoundView();
+        cardPanel.add(noAddressFoundView, noAddressFoundView.getViewName());
+        return this;
+    }
+
+    public AppBuilder addGeocoderUseCase() {
+
+        GeocodingPresenter geocodingPresenter = new GeocodingPresenter(viewManager, displayGeocoderView);
+        GeocodingInteractor geocodingInteractor = new GeocodingInteractor(geocoderDAO, geocodingPresenter);
+        GeocodingController geocodingController = new GeocodingController(geocodingInteractor, geocodingPresenter);
+
+        displayGeocoderView.setDisplayStarChartController(geocodingController);
+
+        geocoderView.setGeoCodingController(geocodingController);
+        noAddressFoundView.setGeocodingController(geocodingController);
+
         return this;
     }
 
