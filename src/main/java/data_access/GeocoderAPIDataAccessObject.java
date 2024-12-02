7877602e99622d7e470c239d842dea35e4004f2e
alexxxtl <alextl.young@mail.utoronto.ca>
2024-12-02 14:57:02 -0500

    Refactor

--- src/main/java/data_access/GeocoderAPIDataAccessObject.java
+++ src/main/java/data_access/GeocoderAPIDataAccessObject.java
@@ -4,5 +4,5 @@ import java.util.List;
 
 public interface GeocoderAPIDataAccessObject {
 
-    public abstract List<String> converter(String address);
+    List<String> converter(String address);
 }
